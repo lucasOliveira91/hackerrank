ce59e7abff83163cfc1c5fefb80bc1f70a06b5b9
Lucas Oliveira <lucas.oliveira@basis.com.br>
2019-02-28 22:07:59 -0300

    adding

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+e
+5src/language/easy/ValidUserNameRegularExpression.java,d/1/d1e7d24ad9b5dd4447559eb91290a6caa2335562
+O
+src/hire/easy/SockMerchant.java,9/1/916d98d75211d7994f40b5d55bdd03e7b1db04db
---
+++ src/hire/easy/SockMerchant.java
@@ -0,0 +1,33 @@
+package hire.easy;
+
+/**
+ * Created by loliveira on 28/02/19.
+ */
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SockMerchant {
+
+
+
+    public static void main(String[] args) throws IOException {
+        Integer ar[] = {10,20,20,10,20};
+
+        Set set = new HashSet<>();
+
+        int pairs = 0;
+        for (int i = 0; i < ar.length; i++) {
+            if(!set.contains(ar[i])) {
+                set.add(ar[i]);
+            }else {
+                pairs ++;
+                set.remove(ar[i]);
+            }
+        }
+
+        System.out.print(pairs);
+
+    }
+}
